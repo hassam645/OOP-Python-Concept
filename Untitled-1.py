7cd2c74baf05d5325278f9d709291ff8bdd17fd0
hassam bin shahid <139114049+hassam645@users.noreply.github.com>
2024-05-20 21:16:31 -0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+class Employee:
+    def _init_(self,sal,age):
+        self.salary=sal;
+e1=Employee(230000,34)
+print(e1._dict_)
---
+++ oop.py
@@ -0,0 +1,5 @@
+class Email:
+    pass
+e1=Email;
+e2=Email;
+print(type(Email))
